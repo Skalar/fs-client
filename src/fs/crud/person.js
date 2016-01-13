13db9dc2b5eb5a55f278332e36109771e89fde6f
Gudleik Rasch <gudleik@gmail.com>
2016-01-13 16:23:40 +0100

    Added more fields to person model
    Added email, address and phone

--- src/fs/crud/person.js
+++ src/fs/crud/person.js
@@ -3,8 +3,13 @@ import Base from './base'
 export default class Person extends Base {
   static format(data) {
     // Add ssn and name attributes
-    data.ssn  = data.Fodselsdato + data.Personnr
-    data.name = [data.Fornavn, data.Etternavn].join(' ')
+    data.ssn     = data.Fodselsdato + data.Personnr
+    data.name    = [data.Fornavn, data.Etternavn].join(' ').trim()
+    data.email   = data.Emailadresse_Privat || data.Emailadresse
+    data.phone   = data.Telefonnr_Mobil || data.Telefonnr_Hjemsted
+    data.address = ([data.Adrlin1_Hjemsted, data.Adrlin2_Hjemsted].join(' ') + ', ' + data.Adrlin3_Hjemsted).trim()
+    if (data.address === ',') data.address = null
+
     return data
   }
 
